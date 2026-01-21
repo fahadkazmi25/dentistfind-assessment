6636032936dfba1c36cd45e660edc74549da2b0b
fahadkazmi25 <syedfahadalikazmi04@gmail.com>
2026-01-21 16:03:26 +0500

    font changed to outfit

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${outfit.variable} ${geistMono.variable} antialiased font-sans`}
+        className={`${outfit.className} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
